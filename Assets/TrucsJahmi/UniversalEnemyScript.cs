8f0a050907d728f9aa69d7c811b90ba057ac3dd3
NzrManoe <m.nezer2@rubika-edu.re>
2025-11-19 09:19:59 +0400

    modif script enemy

--- Assets/TrucsJahmi/UniversalEnemyScript.cs
+++ Assets/TrucsJahmi/UniversalEnemyScript.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using System.Collections;
-//using TMPro;
+
 public class UniversalEnemyScript : MonoBehaviour
 {
     [Header("Objets generaux")]
     public GameObject target;
-
     public GameObject instantiator;
     public GameObject model3D;
-    Enemy3DModelScript scriptModel3D;
+    private Enemy3DModelScript scriptModel3D;
+
     public int groundLayer;
     public int climbableLayer;
     public int enemyLayer;
@@ -21,7 +21,7 @@ public class UniversalEnemyScript : MonoBehaviour
     public float currentHealthPercentage;
     public float oldHealthPercentage;
     public float oldHP;
-    
+
     [Header("Degats")]
     public float baseDamage;
     public float scaledBaseDamage;
@@ -29,7 +29,7 @@ public class UniversalEnemyScript : MonoBehaviour
     public float attackTimer;
     public float attackRate;
     public float attackReach;
-    bool canAttack;
+    private bool canAttack;
 
     [Header("Mouvements")]
     public float movementSpeed;
@@ -40,192 +40,214 @@ public class UniversalEnemyScript : MonoBehaviour
     [Header("Capacites speciales")]
     public float minEnemySizeMultiplier;
     public float maxEnemySizeMultiplier;
-    public float enemySizeMultiplier; // si c'est 0 les ennemis n'aparaitrons pas /!\
+    public float enemySizeMultiplier;
 
     public bool canMerge;
     public float maxMergeSize;
+
     [Header("Afficher degats subits")]
     public GameObject billboard;
     public Color colorOfTakenDamage;
+
     void Start()
     {
         StartCoroutine("SecondTimer");
         scriptModel3D = model3D.GetComponent<Enemy3DModelScript>();
+        rb.useGravity = true;
         isWalking = true;
-        rb.useGravity = true;        
 
+        // Taille aléatoire
         if (maxEnemySizeMultiplier != minEnemySizeMultiplier)
-        {
             enemySizeMultiplier = Random.Range(minEnemySizeMultiplier, maxEnemySizeMultiplier);
-        }
+
         if (enemySizeMultiplier != 1)
-        {
             transform.localScale *= enemySizeMultiplier;
-        }
+
+        // HP initiaux
         baseHealthPoint *= transform.localScale.x;
         currentHealthPoint = baseHealthPoint;
-        oldScaledBaseHealthPoint = baseHealthPoint;
         scaledBaseHealthPoint = baseHealthPoint;
-        oldHP = currentHealthPoint;
+        oldScaledBaseHealthPoint = scaledBaseHealthPoint;
+
         currentHealthPercentage = 100;
-        oldHealthPercentage = currentHealthPercentage;
-        //model3D.GetComponent<Enemy3DModelScript>().normalSize = transform.localScale.x;
-        //Debug.Log(transform.localScale);
+        oldHealthPercentage = 100;
+        oldHP = currentHealthPoint;
+
         ScaleStatsWithSize();
     }
-    void Update()
-    {
 
-        if (!canAttack && attackTimer < attackRate)
+    void Update() // plus sympa a voir le update()
+    {
+        HandleAttackTimer();
+        HandleDamageFeedback();
+        HandleMovement();
+        HandleDeath();
+    }
+
+    // -----------------------------
+    //     SYSTEMES PRINCIPAUX
+    // -----------------------------
+
+    void HandleAttackTimer()
+    {
+        if (!canAttack)
         {
             attackTimer += Time.deltaTime;
+            if (attackTimer >= attackRate)
+            {
+                attackTimer = 0;
+                canAttack = true;
+            }
         }
-        else
-        {
-            attackTimer = 0;
-            canAttack = true;
-            //Debug.Log("dist");
-        }
+    }
 
-        if (currentHealthPoint < oldHP) // calculer les degats qu'on a subit
-        { 
-            currentHealthPercentage = currentHealthPoint * 100 / scaledBaseHealthPoint;
-            // afficher les degats
-            var instantiated = Instantiate(billboard, transform.position, Quaternion.identity);
-            var instantiatedScript = instantiated.GetComponent<BillboardSpriteScript>();
-            instantiatedScript.billboardText.text = ((int)(oldHP - currentHealthPoint)).ToString();
-            instantiatedScript.instantiatorSizeOffset = transform.localScale.z / 2;
-            instantiatedScript.color = colorOfTakenDamage;
-            oldHP = currentHealthPoint;
-            //model3D.GetComponent<Enemy3DModelScript>().tookAHit = true;
+    void HandleDamageFeedback()
+    {
+        if (currentHealthPoint >= oldHP) return;
 
-            scriptModel3D.TookDamage(oldHealthPercentage - currentHealthPercentage); // force du coup
-            oldHealthPercentage = currentHealthPercentage;
-        }
-        if (isWalking && fallTimer > 0) // timer pour permettre au rb de grimper les bords de mesh, sans ca il retombe arrive en haut
-        {
-            fallTimer -= 10 * Time.deltaTime; // c'est moche, pas tres elegant
-        }
-        else
-        {
-            rb.useGravity = true;
-        }
-        var targetDir = target.transform.position - transform.position; // la direction de la cible par rapport a nous
-        transform.rotation = Quaternion.LookRotation(new Vector3(targetDir.x, 0, targetDir.z)); // on s'oriente vers la cible uniquement dans l'axe x,z        
-        if(isWalking)
+        currentHealthPercentage = currentHealthPoint * 100 / scaledBaseHealthPoint;
+
+        // Affichage des dégâts
+        var inst = Instantiate(billboard, transform.position, Quaternion.identity);
+        var instScript = inst.GetComponent<BillboardSpriteScript>();
+        instScript.billboardText.text = ((int)(oldHP - currentHealthPoint)).ToString();
+        instScript.instantiatorSizeOffset = transform.localScale.z / 2;
+        instScript.color = colorOfTakenDamage;
+
+        scriptModel3D.TookDamage(oldHealthPercentage - currentHealthPercentage);
+
+        oldHealthPercentage = currentHealthPercentage;
+        oldHP = currentHealthPoint;
+    }
+
+    void HandleMovement()
+    {
+        var targetDir = target.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(new Vector3(targetDir.x, 0, targetDir.z));
+
+        if (isWalking)
         {
             Walk();
             GroundDetection();
         }
         else
         {
-            //transform.position += (transform.up - (transform.forward * 0.5f)) * movementSpeed * Time.deltaTime;
-            transform.position += transform.up * movementSpeed * Time.deltaTime; // si on touche un mur on monte
-            fallTimer = 1;
+            // grimpe
             rb.useGravity = false;
+            transform.position += transform.up * movementSpeed * Time.deltaTime;
+            fallTimer = 1;
         }
-        
-        if (currentHealthPoint <= 0)
-        {
-            Destruction();
-        }
-        if (transform.position.y < -5)
-        {
-            Destruction();
-        }
+
+        if (fallTimer > 0)
+            fallTimer -= 10 * Time.deltaTime;
+        else
+            rb.useGravity = true;
     }
+
+    void HandleDeath()
+    {
+        if (currentHealthPoint <= 0 || transform.position.y < -5)
+            Destruction();
+    }
+
+    // -----------------------------
+    //     COLLISIONS
+    // -----------------------------
+
     void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.layer == climbableLayer)
+        int layer = collision.gameObject.layer;
+
+        if (layer == climbableLayer)
         {
-            transform.position -= new Vector3(collision.GetContact(0).point.x - transform.position.x, 0, collision.GetContact(0).point.z - transform.position.z) * movementSpeed * Time.deltaTime; // on s'eloigne du point de contact du mur touche pour pouvoir grimper a plus de 90 degres
+            var point = collision.GetContact(0).point;
+            transform.position -= new Vector3(point.x - transform.position.x, 0, point.z - transform.position.z) * movementSpeed * Time.deltaTime;
             isWalking = false;
         }
-        if (canMerge && transform.localScale.x < maxMergeSize && collision.gameObject.layer == enemyLayer) // manger les autres enemis plus petits que soit et grossir proportionellement
+
+        if (canMerge && layer == enemyLayer && transform.localScale.x < maxMergeSize)
         {
-            if (collision.gameObject.transform.localScale.x < transform.localScale.x)
+            var other = collision.gameObject.transform;
+            if (other.localScale.x < transform.localScale.x)
             {
                 if (transform.localScale.x < 1)
-                {
-                    transform.localScale += collision.gameObject.transform.localScale / 2; // plus il est gros moins il grossit
-                }
+                    transform.localScale += other.localScale / 2;
                 else
-                {
-                    transform.localScale += collision.gameObject.transform.localScale / transform.localScale.x; // plus il est gros moins il grossit
-                }
+                    transform.localScale += other.localScale / transform.localScale.x;
+
                 collision.gameObject.GetComponent<UniversalEnemyScript>().Destruction();
                 ScaleStatsWithSize();
             }
         }
     }
+
     void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.layer == climbableLayer)
-        {
             isWalking = true;
-        }
     }
+
+    // -----------------------------
+    //     FONCTIONS UTILITAIRES
+    // -----------------------------
+
     void Walk()
     {
-        transform.position += transform.forward * movementSpeed * Time.deltaTime; // on avance vers l'avant        
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
     }
+
     float CalculateDistanceFromTarget()
     {
-        float distance = (target.transform.position - transform.position).sqrMagnitude;
-        return distance;
+        return (target.transform.position - transform.position).sqrMagnitude;
     }
+
     void GroundDetection()
     {
-        //RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, 0.65f)) // on utilise un raycast au lieu d'un spherecast car c'est suffisant pour l'IA des enemis
+        if (Physics.Raycast(transform.position, -transform.up, 0.65f))
         {
             fallTimer = 1;
             rb.useGravity = false;
         }
-        //Debug.DrawLine(transform.position, transform.position - transform.up * 0.65f, Color.red);
     }
-    void ScaleStatsWithSize() // augmente les statistiques de l'ennemi selon la nouvelle taille en gardant les proportions actuelles
+
+    void ScaleStatsWithSize()
     {
-        scaledBaseHealthPoint = (baseHealthPoint * transform.localScale.x) * transform.localScale.x;
-        float newCurrentHP = (currentHealthPoint * scaledBaseHealthPoint) / oldScaledBaseHealthPoint;
-        currentHealthPoint = newCurrentHP;
+        scaledBaseHealthPoint = baseHealthPoint * transform.localScale.x * transform.localScale.x;
+
+        currentHealthPoint = (currentHealthPoint * scaledBaseHealthPoint) / oldScaledBaseHealthPoint;
         oldScaledBaseHealthPoint = scaledBaseHealthPoint;
+
         scaledBaseDamage = baseDamage * transform.localScale.x * transform.localScale.x;
         damageToDeal = scaledBaseDamage;
+
         oldHP = currentHealthPoint;
+
         attackReach = transform.localScale.x * 0.75f;
-        //model3D.GetComponent<Enemy3DModelScript>().normalSize = transform.localScale.x;
     }
+
     public void Destruction()
     {
-        instantiator.GetComponent<EnemySpawnerScript>().enemiesExisting.Remove(gameObject); // on se retire des listes avant de partir vers d'autres cieux :')
+        instantiator.GetComponent<EnemySpawnerScript>().enemiesExisting.Remove(gameObject);
         Destroy(gameObject);
     }
-    
+
     void Attack()
     {
-        if(canAttack)
-        {
-            if (damageToDeal <= 0)
-            {
-                damageToDeal = 1;
-            }
-            target.GetComponent<PlayerStatsScript>().TakeDamage(damageToDeal);
-            canAttack = false;
-            //Debug.Log("dist");
-        }
+        if (!canAttack) return;
+
+        if (damageToDeal <= 0)
+            damageToDeal = 1;
+
+        target.GetComponent<PlayerStatsScript>().TakeDamage(damageToDeal);
+        canAttack = false;
     }
 
     IEnumerator SecondTimer()
     {
-        float dist = CalculateDistanceFromTarget();
-        if (dist <= attackReach * attackReach) // comme on utilise sqrMagnitude au lieu de magnitude la distance obtenue est au carre donc on compare au carre, c'est moins couteux que faire une racine carre
-        {
-            
+        if (CalculateDistanceFromTarget() <= attackReach * attackReach)
             Attack();
-        }
+
         yield return new WaitForSeconds(0.1f);
-        
         StartCoroutine("SecondTimer");
     }
 }
